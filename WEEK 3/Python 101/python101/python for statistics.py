a7fd727178a0be80dba5efb7d6789007730bb16e
MabelOlivia <mabelkarani55@gmail.com>
2020-12-01 23:09:30 +0300

    Week 4

--- WEEK 3/Python 101/python101/python for statistics.py
+++ WEEK 3/Python 101/python101/python for statistics.py
@@ -10,3 +10,5 @@ print("Mean is:", statistics.mean(rang))
 print("Median is:", statistics.median(rang))
 print("Mode is:", statistics.mode(rang))
 
+import datetime
+print(datetime.datetime(1970, 1, 1).strftime('%Y-%d-%B')) 
